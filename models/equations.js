d67d1490dd9c27057c893752bd17f159fccfc988
William Dyson <william.dyson@live.com>
2014-04-16 00:33:29 +0100

    added dummy model equations.js and testing route to site.js

---
+++ models/equations.js
@@ -0,0 +1,6 @@
+
+this.getTest1 = function() {
+    return { "id": 5, "name": "test", "latex": "3+5" };
+}
+
+module.exports = this;
--- routes/site.js
+++ routes/site.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var equations = require('../models/equations');
+
 router.get('/', function(req, res) {
     res.render('site/index', { title: 'Test Page' });
+});
+
+router.get('/:id1,:id2', function(req, res) {
+    var id1 = req.params.id1;
+    var id2 = req.params.id2;
+
+    res.send(equations.getTest1().name);
 });
 
 module.exports = router;
