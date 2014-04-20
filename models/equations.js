59409404633f92617b754f3772f4827df4b9bcf1
William Dyson <william.dyson@live.com>
2014-04-20 18:22:06 +0100

    cleaned up callback

--- models/equations.js
+++ models/equations.js
@@ -9,15 +9,8 @@ connection.connect();
 
 this.getFirst = function(callback) {
     connection.query("SELECT * FROM Equation LIMIT 1", function(err, rows, fields) {
-        if(err) {
-            callback(err, null);
-            return;
-        }
-
-        if(rows == undefined || rows.length == 0) {
-            callback(new Error("No rows selected from Equation!"), null);
-            return;
-        }
+        if(err) return callback(err, null);
+        if(rows == undefined || rows.length == 0) return callback(new Error("No rows selected from Equation!"), null);
 
         callback(null, rows[0]);
     });
@@ -27,15 +20,8 @@ this.getFromId = function(id, callback) {
     var id = parseInt(id);
 
     connection.query("SELECT * FROM Equation WHERE id=? LIMIT 1", [id], function(err, rows, fields) {
-        if(err) {
-            callback(err, null);
-            return;
-        }
-
-        if(rows == undefined || rows.length == 0) {
-            callback(new Error("No rows selected from Equation!"), null);
-            return;
-        }
+        if(err) return callback(err, null);
+        if(rows == undefined || rows.length == 0) return callback(new Error("No rows selected from Equation!"), null);
 
         callback(null, rows[0]);
     });
@@ -46,15 +32,8 @@ this.getPairFromId = function(id1, id2, callback) {
     var id2 = parseInt(id2);
 
     connection.query("SELECT * FROM Equation WHERE id=? OR id=?", [id1, id2], function(err, rows, fields) {
-        if(err) {
-            callback(err, null, null);
-            return;
-        }
-
-        if(rows == undefined || rows.length < 2) {
-            callback(new Error("No rows selected from Equation!"), null, null);
-            return;
-        };
+        if(err) return callback(err, null, null);
+        if(rows == undefined || rows.length < 2) return callback(new Error("No rows selected from Equation!"), null, null);
 
         if(rows[0].id == id1) callback(null, rows[0], rows[1]);
         else callback(null, rows[1], rows[0]);
